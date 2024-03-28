247577170ffc1f4d8ce80de0cbf73cc9ed98cde1
Jesse Swart <jesseswart08@gmail.com>
2024-03-28 17:02:37 +0100

    Added the method that will fire the event

--- src/main/java/com/jesse/gravitytax/PlayerMoveListener.java
+++ src/main/java/com/jesse/gravitytax/PlayerMoveListener.java
@@ -1,6 +1,12 @@
 package com.jesse.gravitytax;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerMoveListener implements Listener {
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+
+    }
 }
